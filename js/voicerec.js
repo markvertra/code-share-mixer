8f14ae295962e9e3e7115a32c00daefb392d4071
Mark Allen <mark@traversify.com>
2017-09-16 18:31:59 +0200

    More tags added

--- js/voicerec.js
+++ js/voicerec.js
@@ -28,9 +28,65 @@ $(document).ready(function(){
       },
       'close tag one': function() {
         console.log("CLOSE TAG ONE");
-        $("#pen-HTML").val($("#pen-HTML").val() + "<h1>");
-      }
-      
+        $("#pen-HTML").val($("#pen-HTML").val() + "</h1>");
+      },
+      'tag two': function() {
+        console.log("TAG TWO");
+        $("#pen-HTML").val($("#pen-HTML").val() + "<h2>");
+      },
+      'close tag two': function() {
+        console.log("CLOSE TAG TWO");
+        $("#pen-HTML").val($("#pen-HTML").val() + "</h2>");
+      },
+      'tag three': function() {
+        console.log("TAG THREE");
+        $("#pen-HTML").val($("#pen-HTML").val() + "<h3>");
+      },
+      'tag three': function() {
+        console.log("CLOSE TAG THREE");
+        $("#pen-HTML").val($("#pen-HTML").val() + "</h3>");
+      },
+      'tag four': function() {
+        console.log("TAG FOUR");
+        $("#pen-HTML").val($("#pen-HTML").val() + "<h4>");
+      },
+      'close tag four': function() {
+        console.log("CLOSE TAG FOUR");
+        $("#pen-HTML").val($("#pen-HTML").val() + "</h4>");
+      },
+      'tag five': function() {
+        console.log("TAG FIVE");
+        $("#pen-HTML").val($("#pen-HTML").val() + "<h5>");
+      },
+      'close tag five': function() {
+        console.log("CLOSE TAG FIVE");
+        $("#pen-HTML").val($("#pen-HTML").val() + "</h5>");
+      },
+      'tag six': function() {
+        console.log("TAG SIX");
+        $("#pen-HTML").val($("#pen-HTML").val() + "<h6>");
+      },
+      'close tag six': function() {
+        console.log("TAG SIX");
+        $("#pen-HTML").val($("#pen-HTML").val() + "</h6>");
+      },
+      'tag p': function() {
+        console.log("TAG P");
+        $("#pen-HTML").val($("#pen-HTML").val() + "<p>");
+      },
+      'close tag p': function() {
+        console.log("TAG P");
+        $("#pen-HTML").val($("#pen-HTML").val() + "</p>");
+      },
+      'tag div': function() {
+        console.log("TAG DIV");
+        $("#pen-HTML").val($("#pen-HTML").val() + "<div>");
+      },
+      'close tag div': function() {
+        console.log("CLOSE TAG DIV");
+        $("#pen-HTML").val($("#pen-HTML").val() + "<div>");
+      },
+        '*anything': anything
     };
 
     // Add our commands to annyang
@@ -50,4 +106,8 @@ $(document).ready(function(){
 function selectWriteType(type){
   $(".pen-box").css("border", "1px solid black");
   type.css("border", "1px solid red");
+}
+
+function anything(text) {
+  $("#pen-HTML").val($("#pen-HTML").val() + text);
 }
