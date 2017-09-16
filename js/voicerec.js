594cd1b7e6b08a6ecf82775024e735e64d9a4e84
Mark Allen <mark@traversify.com>
2017-09-16 17:33:31 +0200

    Made British English

--- js/voicerec.js
+++ js/voicerec.js
@@ -3,23 +3,26 @@ $(document).ready(function(){
 
 
   if (annyang) {
-    console.log("annyang")
-    let $writeHTML = $("#pen-HTML");
-    let $writeCSS = $("#pen-CSS");
-    let $writeJS = $("#pen-JS");
+  
+    const $writeHTML = $("#pen-HTML");
+    const $writeCSS = $("#pen-CSS");
+    const $writeJS = $("#pen-JS");
     let $selected; 
     // Let's define our first command. First the text we expect, and then the function it should call
     var commands = {
-      'change HTML': function() {
+      'change page': function() {
         $selected = $writeHTML;
+        selectWriteType($selected);
         console.log("HTML");
       },
-      'change CSS': function() {
+      'change styles': function() {
         $selected = $writeCSS;
+        selectWriteType($selected);
         console.log("CSS");
       },
-      'change JS': function() {
+      'change javascript': function() {
         $selected = $writeJS;
+        selectWriteType($selected);
         console.log("Javascript");
       },
     };
@@ -27,9 +30,15 @@ $(document).ready(function(){
     // Add our commands to annyang
     annyang.addCommands(commands);
 
+    annyang.setLanguage("en-GB");
+
     // Start listening. You can call this here, or attach this call to an event, button, etc.
     annyang.start();
     
   }
 
 });
+
+function selectWriteType(type){
+  type.css("border", "1px solid red");
+}
